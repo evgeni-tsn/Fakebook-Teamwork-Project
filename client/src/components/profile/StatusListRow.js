984d69e2f7bb846ed12efccbd07fa7471d3b8241
Krasimir Nedev <krasi.nedew@gmail.com>
2016-12-07 03:55:06 +0200

    Client side comments pagination

--- client/src/components/profile/StatusListRow.js
+++ client/src/components/profile/StatusListRow.js
@@ -3,17 +3,19 @@ import { Link } from 'react-router'
 import moment from 'moment'
 import { connect } from 'react-redux'
 
+const perPage = 5
+
 class StatusListRow extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      input: ''
+      commentPage: 1,
     }
 
     this.handleDelete = this.handleDelete.bind(this)
     this.handleComment = this.handleComment.bind(this)
-    this.onInput = this.onInput.bind(this)
+    this.showMoreComments = this.showMoreComments.bind(this)
   }
 
   handleDelete() {
@@ -23,15 +25,15 @@ class StatusListRow extends React.Component {
   }
 
   handleComment () {
-    if(this.state.input) {
+    if(this.refs.CommentInput.value) {
+      this.props.comment(this.props.status._id, this.refs.CommentInput.value)
       this.refs.CommentInput.value = ''
-      this.setState({input: ''})
-      this.props.comment(this.props.status._id, this.state.input)
     }
   }
 
-  onInput(e) {
-    this.setState({input: e.target.value})
+  showMoreComments() {
+    this.setState({commentPage: this.state.commentPage + 1})
+    this.forceUpdate()
   }
 
   render() {
@@ -43,7 +45,7 @@ class StatusListRow extends React.Component {
         <div className="panel-heading text-wrap">Last active: {moment(this.props.status.updatedAt).fromNow()}</div>
         <div className="panel-body text-wrap">{this.props.status.content}</div>
         <ul className="ul-no-bullets">
-          {this.props.status.comments.map(function (comment, i) {
+          {this.props.status.comments.slice(0, this.state.commentPage * perPage).map(function (comment, i) {
             return <li key={i} className="text-wrap">
               <Link to={`/${comment.user.username}`} className="comment-user-link text-wrap">
                 {comment.user.username}:
@@ -51,10 +53,15 @@ class StatusListRow extends React.Component {
                 <span className="comment-content text-wrap"> {comment.content} <span className="text-wrap comment-content fr mgr20">{moment(comment.createdAt).fromNow()}</span></span>
               </li>
           })}
+
+          {this.props.status.comments.length > this.state.commentPage * perPage ?
+            <div>
+              <span onClick={this.showMoreComments} className="cursor-pointer">...More</span>
+            </div> : null}
         </ul>
         {this.props.comment && this.props.auth && this.props.auth.isAuthenticated ? <div className="row">
         <span className="col-lg-10 col-md-10 col-sm-8 col-xs-8">
-          <input ref="CommentInput" type="text" className="form-control" placeholder="What do you think?" onChange={this.onInput}/>
+          <input ref="CommentInput" type="text" className="form-control" placeholder="What do you think?"/>
         </span>
           <span className="col-lg-2 col-md-2 col-sm-4 col-xs-4">
           <button className="btn btn-primary btn-sm comment-button" onClick={this.handleComment}>Comment</button>
